32be92963f5e35e9851db39c5620235d3211f90e
YongwooKim <keepingitflow@gmail.com>
2020-10-20 22:09:48 +0900

    docs: Renamed components of the project

--- Available_Recipes/Available_RecipesTests/Available_RecipesTests.swift
+++ Available_Recipes/Available_RecipesTests/Available_RecipesTests.swift
@@ -6,9 +6,9 @@
 //
 
 import XCTest
-@testable import Generous_Recipe
+@testable import Available_Recipes
 
-class Generous_RecipeTests: XCTestCase {
+class Available_RecipesTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
