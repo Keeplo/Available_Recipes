ba71fe41229d3aab91bfb7704155a01b43e039c2
YongwooKim <keepingitflow@gmail.com>
2020-11-04 13:50:59 +0900

    docs: 섹션 열거형생성

---
+++ GenerousRecipe/GenerousRecipe/models/Section.swift
@@ -0,0 +1,17 @@
+//
+//  Section.swift
+//  GenerousRecipe
+//
+//  Created by Yongwoo Marco on 2020/11/04.
+//  Copyright © 2020 FastCampus. All rights reserved.
+//
+
+import Foundation
+
+enum Section: Int, CaseIterable, Codable, Equatable {
+    case nokind
+    case korean
+    case japanese
+    case chinese
+    case western
+}
